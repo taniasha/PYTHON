183cdca3c26b87a5e64133176ac6ddd2b1db94a6
tania sharma <bishupandit07@gmail.com>
2025-09-15 21:35:16 +0530

    Expo and area of circle

---
+++ Project1/Area.py
@@ -0,0 +1,9 @@
+# Area of circle :
+
+
+pi = 3.14
+radius = float(input("Enter the radius: "))
+# area = pi*radius*radius
+# circum = 2*pi*radius
+print(f"Area is : {pi*radius*radius}")
+print(f"Paramter/Circum is : {2*pi*radius}")
