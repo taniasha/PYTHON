7a58cb1c8c897989317389cd519d9c9c6701a233
tania sharma <bishupandit07@gmail.com>
2025-09-20 12:39:03 +0530

    List functions

---
+++ Project1/List.py
@@ -0,0 +1,23 @@
+# Ek program banao jisme ek list create karo aur usme append(), insert(), remove(), pop(), sort() aur reverse() functions ka use dikhayo.
+
+list = ['apple','orange', 'banana']
+
+# append -- add elem at last
+list.append('guava')
+print(list)
+
+# insert --- insert at specified index
+list.insert(2,'melon')
+print(list)
+
+# remove -- remove specific elem
+list.remove("guava")
+print(list)
+
+# pop -- remove index elem
+list.pop(0)
+print(list)
+
+# sort
+list.sort()
+print(list)
--- Project1/expo.py
+++ Project1/expo.py
@@ -1,5 +1,5 @@
 # Square and cube of a number
 
-number = int(input("ENter a number"))
+number = int(input("Enter a number"))
 print("Square of the number is",number**2)
 print("Cube of the number is",number**3)
