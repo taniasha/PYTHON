c7658aa29ddab004cf45cae0cff3ff1b48f0fd81
tania sharma <bishupandit07@gmail.com>
2025-09-20 12:47:56 +0530

    Tuple index, count, max, min, sum functions

---
+++ Project1/Tuple.py
@@ -0,0 +1,25 @@
+# Ek program banao jisme ek tuple create karke count() aur index(), len() functions ka use karke output dikhayo.
+
+tuple = ('white', 'red', 'black', 'red')
+print(tuple)
+
+# count() -- Counts how many times a value appears.
+print(tuple.count('red'))
+
+# index -- Gives the position of the first matching value.
+print(tuple.index('red'))
+
+# len
+print(len(tuple)) 
+# print(tuple.len())--wrong syntax
+
+
+# -----------------------------------------------------------------------------------------------
+
+# if a tuple has numbers then workable functions are :
+t = (2,3,9)
+
+print(min(t))
+print(max(t))
+print(sum(t))
+
