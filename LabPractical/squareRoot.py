0a0c5dba5f0f9aec391c0afafe7d3de54ecc8870
tania sharma <bishupandit07@gmail.com>
2025-11-23 14:12:03 +0530

    Sqrt of a num

---
+++ LabPractical/squareRoot.py
@@ -0,0 +1,13 @@
+# Method1:
+
+num = float(input("Enter number :"))
+sqrt = num ** 0.5
+
+print(f"The sqrt of {num} is {sqrt}")
+
+
+# Method2:
+
+import math
+sqrt1 = math.sqrt(16)
+print(f"Method 2:-- Sqrt of num is: {sqrt1}")
