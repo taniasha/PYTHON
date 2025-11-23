0e229097f4046f67a64dcfe3497ea77d1e620992
tania sharma <bishupandit07@gmail.com>
2025-11-23 14:42:31 +0530

    exponenet

---
+++ LabPractical/exponent.py
@@ -0,0 +1,13 @@
+# Method 1:
+base = int(input('Enter base: '))
+expo = int(input("ENter exponenetial: "))
+
+result = base ** expo
+print("Result is : ", result)
+
+
+# Method 2:
+import math
+
+result = math.pow(base, expo)
+print("Method 2 --> result ", result)
